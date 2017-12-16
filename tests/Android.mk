0f313dd0fed568f1e72333868591dd4277d680ef
Xu Guangxin <guangxin.xu@intel.com>
2017-12-16 08:18:38 -0500

    android: enable yamivpp

--- tests/Android.mk
+++ tests/Android.mk
@@ -1,64 +1,38 @@
 LOCAL_PATH := $(call my-dir)
 
-## v4l2decoder
 include $(CLEAR_VARS)
 include $(LOCAL_PATH)/../common.mk
 
 LOCAL_SRC_FILES := \
-        decodehelp.cpp \
-        decodeinput.cpp \
-        vppinputoutput.cpp \
-        v4l2decode.cpp
+    decodeinput.cpp \
+    decodeoutput.cpp \
+    encodeinput.cpp \
+    vppinputdecode.cpp \
+    vppinputdecodecapi.cpp \
+    vppinputoutput.cpp \
+    vppoutputencode.cpp \
+    vpp.cpp
 
 LOCAL_C_INCLUDES:= \
         $(LOCAL_PATH)/.. \
-        external/libcxx/include \
         $(LOCAL_PATH)/../interface \
+        $(LOCAL_PATH)/../tests \
+        external/libcxx/include \
         $(TARGET_OUT_HEADERS)/libva
 
 LOCAL_SHARED_LIBRARIES := \
-        libyami_v4l2 \
         libutils \
+        liblog \
+        libc++ \
+        libva \
+        libva-android \
         libgui \
-
-ifeq ($(ENABLE-V4L2-OPS),true)
-LOCAL_SHARED_LIBRARIES += libdl
-endif
+        libhardware \
+        libyami \
 
 LOCAL_CPPFLAGS += \
-         -fpermissive
+        -frtti
 
-ifeq ($(ENABLE-V4L2-OPS),true)
-LOCAL_CPPFLAGS += -D__ENABLE_V4L2_OPS__
-endif
-
-LOCAL_MODULE := v4l2decode
-include $(BUILD_EXECUTABLE)
-
-## v4l2encoder
-include $(CLEAR_VARS)
-include $(LOCAL_PATH)/../common.mk
-
-LOCAL_SRC_FILES := \
-        encodeinput.cpp \
-        encodeInputSurface.cpp \
-        v4l2encode.cpp
-
-LOCAL_C_INCLUDES:= \
-        $(LOCAL_PATH)/.. \
-        external/libcxx/include \
-        $(LOCAL_PATH)/../interface \
-        $(TARGET_OUT_HEADERS)/libva
-
-LOCAL_SHARED_LIBRARIES := \
-        libyami_v4l2 \
-        libutils \
-        libgui \
-        libui \
-        liblog
-
-LOCAL_CPPFLAGS += \
-         -fpermissive
-
-LOCAL_MODULE := v4l2encode
+LOCAL_PROPRIETARY_MODULE := true
+LOCAL_MODULE := yamivpp
 include $(BUILD_EXECUTABLE)
